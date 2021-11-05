42ec723753171344eb7e0b303440eee569446a58
deepaksonu <deepaksonu16@gmail.com>
2021-11-05 13:07:51 +0530

    Initial build

---
+++ CommonService/src/main/java/com/deepak/CommonService/events/PaymentProcessedEvent.java
@@ -0,0 +1,13 @@
+package com.deepak.CommonService.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentProcessedEvent {
+    private String paymentId;
+    private String orderId;
+}
